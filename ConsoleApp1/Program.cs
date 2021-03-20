d9ab3cbe13c2f72b83e713b397281d7937f9091d
Nadeem <nadeemch15@yahoo.com>
2021-03-20 21:35:32 +0000

    1st commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            pr.Excep("Test1");
+
+
+        }
+        public void Excep (string st)
+        {
+            switch (st.ToUpper())
+            {
+                case "Test1":
+                    break;
+                case "Test2":
+                    break;
+                case "Test3":
+                    break;
+                case "Test4":
+                    break;
+                case "Test5":
+                    break;
+                                       
+                default:
+                    CustomException ex = new CustomException("Test is not avaliable");
+                    Console.WriteLine(ex);
+                    break; 
+                    
+
+                   
+            }
+        }
+
+    }
+}
